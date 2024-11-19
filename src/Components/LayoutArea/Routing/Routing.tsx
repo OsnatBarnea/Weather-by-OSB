93c28836ea1294314417a05838533d1b5852e683
OsnatBarnea <osnatbarnea1@gmail.com>
2024-11-19 14:45:08 +0200

    package.json edits

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,24 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./Routing.css";
 import { About } from "../../Pages/About/About";
 import { Page404 } from "../../Pages/Page404/Page404";
 import { Home } from "../../Pages/Home/Home";
 import { WeatherForecast } from "../../Pages/WeatherForecast/WeatherForecast";
-import { useState } from "react";
-import { Menu } from "../Menu/Menu";
 
 export function Routing(): JSX.Element {
-    // const [city, setTheCity] = useState<string>("");
-
     return (
         <div className="Routing">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/Three-days-forecast" element={<WeatherForecast />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<Page404 />} />
-            </Routes>
+            <Router basename="/Weather-by-OSB">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/Three-days-forecast" element={<WeatherForecast />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="*" element={<Page404 />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
